274d7e17164d927dd7bfab92f2029dd7e26f05d5
NghiaTC <congnghia93tdt@gmail.com>
2023-11-12 21:26:54 +0700

    create list employee

---
+++ app/src/main/java/com/team/team_07_fe/ui/employee/EmployeeManagerFragment.java
@@ -0,0 +1,33 @@
+package com.team.team_07_fe.ui.employee;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.team.team_07_fe.R;
+
+
+public class EmployeeManagerFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_employee_manager, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
