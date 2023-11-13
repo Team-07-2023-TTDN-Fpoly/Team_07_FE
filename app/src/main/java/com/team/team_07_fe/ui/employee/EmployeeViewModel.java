660c3dae3811f90ee1d82e0fd8fb6951b703cd41
NghiaTC <congnghia93tdt@gmail.com>
2023-11-13 20:02:30 +0700

    Update Employee

--- app/src/main/java/com/team/team_07_fe/ui/employee/EmployeeViewModel.java
+++ app/src/main/java/com/team/team_07_fe/ui/employee/EmployeeViewModel.java
@@ -54,8 +54,8 @@ public class EmployeeViewModel extends ViewModel {
         WorkShift morningShift = new WorkShift(1, "Ca sáng", LocalTime.of(8, 0), LocalTime.of(16, 0), "Làm việc buổi sáng");
         WorkShift eveningShift = new WorkShift(2, "Ca chiều", LocalTime.of(16, 0), LocalTime.of(0, 0), "Làm việc buổi tối");
 
-        employees.add(new Employee(1, "Nguyen Van A", "0123456789", new Date(), 5000000, "Hà Nội", "Nhân viên", morningShift, new Date(), "a@example.com"));
-        employees.add(new Employee(2, "Tran Thi B", "0987654321", new Date(), 6000000, "TP HCM", "Quản lý", eveningShift, new Date(), "b@example.com"));
+        employees.add(new Employee(0, "Nguyen Van A", "0123456789", new Date(), 5000000, "Hà Nội", "Nhân viên", morningShift, new Date(), "a@example.com"));
+        employees.add(new Employee(1, "Tran Thi B", "0987654321", new Date(), 6000000, "TP HCM", "Quản lý", eveningShift, new Date(), "b@example.com"));
         // Thêm thêm nhân viên mẫu tại đây nếu cần
 
         listEmp.setValue(employees); // Cập nhật MutableLiveData
