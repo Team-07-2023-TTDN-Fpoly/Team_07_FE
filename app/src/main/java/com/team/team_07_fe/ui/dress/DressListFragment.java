5d0bb1ca95e666adeabfeae12d61dce3b4c9a5da
unknown <dunglvpd06617@fpt.edu.vn>
2023-11-18 09:06:33 +0700

    Dung - layout danh sach ao cuoi

---
+++ app/src/main/java/com/team/team_07_fe/ui/dress/DressListFragment.java
@@ -0,0 +1,52 @@
+package com.team.team_07_fe.ui.dress;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.team.team_07_fe.R;
+public class DressListFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public DressListFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static DressListFragment newInstance(String param1, String param2) {
+        DressListFragment fragment = new DressListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dress_list, container, false);
+    }
+}
