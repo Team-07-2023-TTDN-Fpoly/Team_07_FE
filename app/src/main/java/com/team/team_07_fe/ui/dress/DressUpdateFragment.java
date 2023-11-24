98d6582b78153bff980efa92bc15b943ec22f06f
unknown <vubnnpd06546@fpt.edu.vn>
2023-11-24 21:25:12 +0700

    commit new dress

--- app/src/main/java/com/team/team_07_fe/ui/dress/DressUpdateFragment.java
+++ app/src/main/java/com/team/team_07_fe/ui/dress/DressUpdateFragment.java
@@ -1,11 +1,15 @@
 package com.team.team_07_fe.ui.dress;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,16 +17,16 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.widget.ImageViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.team.team_07_fe.MainActivity;
 import com.team.team_07_fe.R;
-import com.team.team_07_fe.models.Customer;
-import com.team.team_07_fe.models.Dress;
-import com.team.team_07_fe.models.Employee;
-import com.team.team_07_fe.utils.FormatHelper;
-import com.team.team_07_fe.utils.LoadingDialog;
 
-import java.util.Date;
+import com.team.team_07_fe.models.Dress;
+
+import com.team.team_07_fe.utils.LoadingDialog;
+import com.team.team_07_fe.viewmodels.DressViewModel;
 
 
 public class DressUpdateFragment extends Fragment {
@@ -56,7 +60,7 @@ public class DressUpdateFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         if(getArguments()!=null){
-            originalData = (Dress) getArguments().getSerializable("data_customer");
+            originalData = (Dress) getArguments().getSerializable("data_dress");
             setData(originalData);
         }
         //Click button
@@ -79,7 +83,7 @@ public class DressUpdateFragment extends Fragment {
             builder.create().show();
         }
     }
-    private void handleUpdateData(View view){
+    private void handleUpdateData(View view) {
         String name = layout_input_name.getEditText().getText().toString().trim();
         String id = layout_input_id.getEditText().getText().toString().trim();
         String type = layout_input_type.getEditText().getText().toString().trim();
@@ -89,20 +93,19 @@ public class DressUpdateFragment extends Fragment {
         String des = layout_input_des.getEditText().getText().toString().trim();
 
 
+        if (validateInput(name, id, price)) {
 
-
-        if(validateInput(name,id,price)){
-
-            Employee employeeRequest = new Employee(name,id,type,color,size,price,des);
-            showDialogConfirmUpdate(id,employeeRequest);
+            Dress dressRequest = new Dress(name, id, type, color, size, price);
+            showDialogConfirmUpdate(id, dressRequest);
         }
+    }
         private void showDialogConfirmUpdate(String id, Dress dressRequest){
             AlertDialog.Builder builder = new AlertDialog.Builder(requireContext())
                     .setTitle("Thông báo!")
                     .setMessage("Bạn có chắc muốn cập nhật áo cưới này không? " +
                             "Mọi thông tin trước đó sẽ không được lưu.")
                     .setPositiveButton(R.string.yes,(dialog, which) -> {
-                        mViewModel.updateEmployee(Integer.parseInt(id),dressRequest);
+                        mViewModel.updateDress(Integer.parseInt(id),dressRequest);
                         refreshFragment();
                         dialog.dismiss();
                     })
@@ -111,5 +114,96 @@ public class DressUpdateFragment extends Fragment {
                     }));
             builder.create().show();
         }
+
+
+    public static void showAlertDialog(Context context, String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("CÓ", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Xử lý khi người dùng bấm OK
+                        dialog.dismiss(); // Đóng dialog
+                    }
+                });
+        builder.setNegativeButton("HỦY", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // xu li khi nguoi dung bam HUY
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    private boolean validateInput(String name,String id, String price){
+        boolean isValid = true;
+
+        if(TextUtils.isEmpty(name)){
+            layout_input_name.setError("Vui lòng nhập tên áo cưới!");
+            isValid = false;
+        }else{
+            layout_input_name.setError(null);
+        }
+
+        if(TextUtils.isEmpty(id) ){
+            layout_input_id.setError("Vui lòng nhập đúng định dạng!");
+            isValid = false;
+        }else{
+            layout_input_id.setError(null);
+        }
+
+        if(TextUtils.isEmpty(price)){
+            layout_input_price.setError("Vui lòng nhập giá áo cưới!");
+            isValid = false;
+        }else{
+            layout_input_price.setError(null);
+        }
+
+
+        return isValid;
+    }
+    private void mapping(View view){
+        layout_input_id = view.findViewById(R.id.layout_input_id);
+        layout_input_name = view.findViewById(R.id.layout_input_name);
+        layout_input_price = view.findViewById(R.id.layout_input_price);
+        layout_input_type = view.findViewById(R.id.layout_input_type);
+        layout_input_color = view.findViewById(R.id.layout_input_color);
+        layout_input_size = view.findViewById(R.id.layout_input_size);
+        layout_input_des = view.findViewById(R.id.layout_input_des);
+        btn_reloadDress = view.findViewById(R.id.btn_reloadDress);
+        btn_updateDress = view.findViewById(R.id.btn_updateDress);
+    }
+    private void setData(Dress dress) {
+        // Set lại thông tin id, nếu có trường hiển thị id
+        if (layout_input_id.getEditText() != null) {
+            layout_input_id.getEditText().setText(dress.getId());
+        }
+        // Set lại tên
+        layout_input_name.getEditText().setText(dress.getDress_name());
+        // Set lại mau
+        layout_input_color.getEditText().setText(dress.getColor());
+        // Set lại gia ao
+        layout_input_price.getEditText().setText(String.valueOf(dress.getDress_price()));
+        // Set lại mo ta
+        layout_input_des.getEditText().setText(dress.getDress_description());
+    }
+    private void refreshFragment(){
+        loadingDialog.dismiss();
+        NavHostFragment.findNavController(DressUpdateFragment.this)
+                .popBackStack();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((MainActivity)requireActivity()).hiddenBottomBar();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((MainActivity) requireActivity()).showBottomBar();
     }
 }
